9e2ab629ae67678bde9b99162021465c7175c3c4
JWEmmanuel89 <90356423+JWEmmanuel89@users.noreply.github.com>
2023-01-29 15:15:44 -0600

    Question 7

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"math"
+)
+
 // #6. Create structure type Circle
 type Circle struct {
 	Radius float64
+}
+
+// #7. Create method Area of type Circle
+func (c Circle) Area() float64 {
+	return math.Pi * (c.Radius * c.Radius)
 }
